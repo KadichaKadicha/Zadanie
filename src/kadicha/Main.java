fa5d5b06328c8059ebfb1153b5ff35f054e1f958
KadichaKadicha <kadichatashieva@mail.ru>
2019-08-22 14:26:41 +0600

    Initial commit

---
+++ src/kadicha/Main.java
@@ -0,0 +1,12 @@
+package kadicha;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Задание на сообразительность
+        String name="Эльза";
+        System.out.println("Здраствуйте!");
+        System.out.println("Как вас  зовут?");
+        System.out.println( "Здрасвтуйте"+ "," +" "+ name+ "!");
+    }
+}
